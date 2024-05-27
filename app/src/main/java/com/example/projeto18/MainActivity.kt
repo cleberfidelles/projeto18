f1099c41a1728cd9bf6929d51c11bde78172458e
cleber <cleber.fidelles@gmail.com>
2024-05-27 20:30:36 -0300

    segundo ponto da historia

--- app/src/main/java/com/example/projeto18/MainActivity.kt
+++ app/src/main/java/com/example/projeto18/MainActivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
